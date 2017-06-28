6f6c2b18b714a3470d303ece183d761fca8806fd
wxidong@Sanai.win7 <amaork@gmail.com>
2017-06-28 11:20:05 +0800

    GPIO support cleanup, deconstructed auto cleanup

--- raspi_io/gpio.py
+++ raspi_io/gpio.py
@@ -2,7 +2,7 @@
 import uuid
 from .client import RaspiWsClient
 from .core import RaspiBasicMsg, RaspiAckMsg
-__all__ = ['GPIO', 'GPIOEvent', 'GPIOChannel', 'GPIOCtrl', 'GPIOSetup', 'GPIOMode',
+__all__ = ['GPIO', 'GPIOEvent', 'GPIOChannel', 'GPIOCtrl', 'GPIOSetup', 'GPIOMode', 'GPIOCleanup',
            'SoftPWM', 'GPIOSoftPWM', 'GPIOSoftPWMCtrl']
 
 
@@ -58,6 +58,14 @@ class GPIOSetup(RaspiBasicMsg):
         super(GPIOSetup, self).__init__(**kwargs)
 
 
+class GPIOCleanup(RaspiBasicMsg):
+    _handle = 'cleanup'
+    _properties = ('channel',)
+
+    def __init__(self, **kwargs):
+        super(GPIOCleanup, self).__init__(**kwargs)
+
+
 class GPIOChannel(RaspiBasicMsg):
     _handle = 'input'
     _properties = ('channel', 'value')
@@ -68,7 +76,7 @@ class GPIOChannel(RaspiBasicMsg):
 
 
 class GPIOSoftPWM(RaspiBasicMsg):
-    _handle = 'pwm'
+    _handle = 'pwm_init'
     _properties = ('mode', 'channel', 'frequency')
 
     def __init__(self, **kwargs):
@@ -102,6 +110,10 @@ class GPIO(RaspiWsClient):
 
     def __init__(self, address, timeout=1):
         super(GPIO, self).__init__(address, timeout)
+        self.__registered = set()
+
+    def __del__(self):
+        self.cleanup(list(self.__registered))
 
     def setmode(self, mode):
         self._transfer(GPIOMode(mode=mode))
@@ -117,11 +129,22 @@ class GPIO(RaspiWsClient):
 
         return data.value
 
+    def cleanup(self, channel):
+        self._transfer(GPIOCleanup(channel=channel))
+
     def output(self, channel, value):
         self._transfer(GPIOCtrl(channel=channel, value=value))
 
     def setup(self, channel, direction, pull_up_down=PUD_OFF, initial=LOW):
-        self._transfer(GPIOSetup(channel=channel, direction=direction, pull_up_down=pull_up_down, initial=initial))
+        ret = self._transfer(
+            GPIOSetup(channel=channel, direction=direction, pull_up_down=pull_up_down, initial=initial)
+        )
+        # Setup success register gpio channel
+        if isinstance(ret, RaspiAckMsg) and ret.ack:
+            if isinstance(channel, (list, tuple)):
+                [self.__registered.add(c) for c in channel]
+            else:
+                self.__registered.add(channel)
 
 
 class SoftPWM(RaspiWsClient):
@@ -130,11 +153,13 @@ class SoftPWM(RaspiWsClient):
     def __init__(self, address, mode, channel, frequency, timeout=1):
         super(SoftPWM, self).__init__(address, timeout)
         self.__state = False
+        self.__channel = channel
         self._transfer(GPIOSoftPWM(mode=mode, channel=channel, frequency=frequency))
         self.uuid = str(uuid.uuid5(uuid.NAMESPACE_OID, '{0:d},{1:d},{2:d}'.format(mode, channel, frequency)))
 
     def __del__(self):
         self.stop()
+        self._transfer(GPIOCleanup(channel=self.__channel))
 
     def start(self, duty):
         ret = self._transfer(GPIOSoftPWMCtrl(uuid=self.uuid, duty=duty))
